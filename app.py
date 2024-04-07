837f5f7cbb61a9fe15580e5d59f867922107b483
subhopiku <165419017+subhopiku@users.noreply.github.com>
2024-04-07 20:47:04 +0000

    Additional changes for website

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from flask import Flask, render_template 
 app = Flask(__name__)
+
+COURSE = [
+  {
+    'id':1,
+     'name':'Data Analysis'
+  },
+  {
+    'id':2,
+     'name':'Data Science'
+  }
+]
+
+
 @app.route("/")
-def hello_world():
-  return render_template('home.html')
+def hello_techyian():
+  return render_template('home.html',course=COURSE)
 
 if __name__ =="__main__":
   app.run(host='0.0.0.0',debug=True)
